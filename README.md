a0c2ec05f264bf29522caf56945f5b8f98b56277
Anton Ödman <anton.odman@gmail.com>
2026-06-14 01:19:10 +0200

    chore: first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# aigent
+
+[![NPM version](https://img.shields.io/npm/v/aigent?color=%23c53635&label=%20)](https://www.npmjs.com/package/aigent)
+
+A simple CLI application template.
+
+## Install
+
+```bash
+npm install aigent -g
+```
+
+## Usage
+
+```bash
+# run
+aigent
+
+# run subcommand
+aigent subcommand <name>
+```
---
+++ src/cli.ts
@@ -0,0 +1,22 @@
+import { defineCommand } from "citty";
+import { version } from "../package.json";
+
+export const main = defineCommand({
+    meta: {
+        name: "aigent",
+        version,
+        description: "Example CLI",
+    },
+    args: {
+        dev: {
+            type: "boolean",
+            description: "Run in development mode",
+            required: false,
+            default: false,
+            alias: "d",
+        },
+    },
+    subCommands: {
+        helloWorld: () => import("@/commands/hello-world").then(m => m.helloWorldCommand),
+    },
+});
